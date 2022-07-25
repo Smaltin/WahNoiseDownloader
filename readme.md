c5d063a9332eceffe07abd613f189d870c49b182
Smaltin <sterling.maltin@gmail.com>
2022-07-25 02:03:38 -0700

    v0.0.1

---
+++ readme.md
@@ -0,0 +1,5 @@
+This will download all 480 "Wah" sounds from https://inanoises.com.
+
+I did this because I'm lazy and don't feel like downloading them manually like another person I know. I hope they're still alive
+
+https://discord.gg/pacifamily
---
+++ src/WahDownloader.java
@@ -0,0 +1,30 @@
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class WahDownloader {
+    public static void main(String[] args) throws IOException {
+        String location = "https://inanoises.com/resources/noises/WAH_%s.mp3";
+        Files.createDirectories(Paths.get("export"));
+        for (int i = 1; i <= 480; i++) {
+            String downl = location.formatted(String.format("%04d", i));
+            downloadFile(new URL(downl), "export/WAH_" + String.format("%04d", i) + ".mp3");
+            System.out.println("Downloaded #" + i + " to " + "export/WAH_" + String.format("%04d", i) + ".mp3");
+        }
+        System.out.println("Finished downloading all 480 files");
+    }
+
+    public static void downloadFile(URL url, String fileName) throws IOException {
+        try (InputStream in = url.openStream(); BufferedInputStream bis = new BufferedInputStream(in); FileOutputStream fos = new FileOutputStream(fileName)) {
+            byte[] data = new byte[1024];
+            int count;
+            while ((count = bis.read(data, 0, 1024)) != -1) {
+                fos.write(data, 0, count);
+            }
+        }
+    }
+}
